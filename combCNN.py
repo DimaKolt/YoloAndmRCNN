20fc6f68eef1683198cfd6f4828b668ddd8c56c0
DimaKolt <dima_klt@yahoo.com>
2020-08-04 08:42:23 -0700

    before merge

--- combCNN.py
+++ combCNN.py
@@ -15,10 +15,10 @@ yolo_try1.readImage(choosen_pic)
 yolo_ress = yolo_try1.predict()
 
 my_predicts = utils.ResultFilter(yolo_ress, mrcnn_ress)
-filtred_by_iou = my_predicts.filter_IOU_and_score(0.90)
+filtred_by_iou = my_predicts.filter_IOU_and_score(0.80)
 
 # yolo_try1.show(yolo_ress)
 yolo_try1.show(filtred_by_iou)
-mrcnn_try1.show(mrcnn_ress) #TODO non blocking pic
+# mrcnn_try1.show(mrcnn_ress) #TODO non blocking pic
 
 coco = 7
